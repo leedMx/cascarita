d5e48013de7411b8fd3b0914fd8d996f92bd3b73
leedMx <esparza.mexico@gmail.com>
2022-12-29 11:20:36 -0600

    Improved Expenses.js by using map()

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,29 +3,12 @@ import "./ExpenseItem";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(_props) {
-  const [e1, e2, e3, e4] = _props.elements;
+  const expenses = _props.expenses.map( e => {
+    return <ExpenseItem title={e.title} amount={e.amount} date={e.date}/>
+  })
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={e1.title}
-        amount={e1.amount}
-        date={e1.date}
-      />
-      <ExpenseItem
-        title={e2.title}
-        amount={e2.amount}
-        date={e2.date}
-      />
-      <ExpenseItem
-        title={e3.title}
-        amount={e3.amount}
-        date={e3.date}
-      />
-      <ExpenseItem
-        title={e4.title}
-        amount={e4.amount}
-        date={e4.date}
-      />
+      {expenses}
     </div>
   );
 }
