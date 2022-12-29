5c72e7e41bf6c8a25f1cd1aa616569b78c051791
leedMx <esparza.mexico@gmail.com>
2022-12-29 03:52:30 -0600

    Implemented ExpenseDate

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(_props) {
+    const month = _props.date.toLocaleString('en-US', {month: 'long'});
+    const day = _props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = _props.date.getFullYear();
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{day}</div>
+            <div className='expense-date__day'>{year}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(_props) {
   return (
     <div className="expense-item">
-      <div>{_props.date.toISOString()}</div>
+      <ExpenseDate date={_props.date} />
       <div className="expense-item__description">
         <h2>{_props.title}</h2>
         <div className="expense-item__price">${_props.amount}</div>
