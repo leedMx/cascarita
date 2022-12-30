49b4a56584b5d3353476e35dfa5e0754d6dfd66f
leedMx <esparza.mexico@gmail.com>
2022-12-29 21:34:50 -0600

    Propagating new expense up to App

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (_props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -25,7 +25,7 @@ const ExpenseForm = () => {
       date: enteredDate,
       amount: enteredAmount,
     };
-    console.log(expenseData);
+    _props.onSaveExpenseData(expenseData);
     setEnteredAmount("");
     setEnteredDate("");
     setEnteredTitle("");
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,11 +2,18 @@ import React from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
-    return <div className="new-expense">
-        <ExpenseForm />
-    </div>
+const NewExpense = (_props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()*100
+        }
+        _props.onAddExpense(expenseData);
+    }
 
+    return <div className="new-expense">
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    </div>
 }
 
 export default NewExpense
