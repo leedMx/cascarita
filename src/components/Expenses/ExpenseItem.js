ec5b160804b3a6ae64484c6b4f4ab7a52cc649d1
leedMx <esparza.mexico@gmail.com>
2022-12-29 23:12:41 -0600

    Removed unnecesary useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (_props) => {
-  const [title, setTitle] = useState(_props.title);
-
-  const clickHandler = (_props) => {
-    setTitle("Updated!");
-    console.log(title)
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={_props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{_props.title}</h2>
         <div className="expense-item__price">${_props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Button</button>
     </Card>
   );
 };
