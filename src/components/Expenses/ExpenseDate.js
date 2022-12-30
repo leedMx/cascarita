778fce53c760414755dec5f2d2b421b5e8fcc764
leedMx <esparza.mexico@gmail.com>
2022-12-29 23:40:12 -0600

    Fix type error from String to Date conversion

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './ExpenseDate.css';
+import React from "react";
+
+import "./ExpenseDate.css";
 
 const ExpenseDate = (_props) => {
-    const month = _props.date.toLocaleString('en-US', {month: 'long'});
-    const day = _props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = _props.date.getFullYear();
-    return (
-        <div className='expense-date'>
-            <div className='expense-date__month'>{month}</div>
-            <div className='expense-date__year'>{day}</div>
-            <div className='expense-date__day'>{year}</div>
-        </div>
-    )
-}
+  const date = new Date(_props.date);
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
 
-export default ExpenseDate
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{day}</div>
+      <div className="expense-date__day">{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
