9a016147e19b28965091d245d4af8e89a38f4f51
leedMx <esparza.mexico@gmail.com>
2022-12-29 22:00:00 -0600

    Style correction

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Expenses.css";
 import "./ExpenseItem";
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card"
-import ExpensesFilter from './ExpensesFilter';
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (_props) => {
   const filteringHandler = (filter) => {
-    console.log(filter)
-  }
+    console.log(filter);
+  };
 
-  const expenses = _props.expenses.map( e => {
-    return <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date}/>
-  })
+  const expenses = _props.expenses.map((e) => {
+    return (
+      <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date} />
+    );
+  });
+
   return (
     <Card className="expenses">
-      <ExpensesFilter onFiltering={filteringHandler}/>
+      <ExpensesFilter onFiltering={filteringHandler} />
       {expenses}
     </Card>
   );
-}
+};
 
-export default Expenses
+export default Expenses;
